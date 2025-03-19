525f3b6bf53915cc1bdb748e7a99684560a9f085
Tyrone Tudehope <tyronetudehope@gmail.com>
2025-03-19 09:52:34 +0200

    chore: Add some comments

--- src/ExtraConfig.php
+++ src/ExtraConfig.php
@@ -11,9 +11,14 @@ class ExtraConfig extends Model
 	public ?string $primarySiteUrl = null;
 
 	/**
+	 * Define any extra aliases
+	 *
 	 * @var array<non-empty-string, ?string>
 	 */
 	public array $aliases = [];
 
+	/**
+	 * If true, any extra aliases will be merged with the existing aliases. Otherwise, aliases defined here will replace existing aliases.
+	 */
 	public bool $mergeAliases = true;
 }
